8dfcc81ffc3bcd4dfec0829e54c5ec18c0fb61d0
Uriel Summonte <seti137@gmail.com>
2024-05-26 16:11:34 -0300

    index finalizado tailwind integrado

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,15 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    colors: {
+      'nav-color': '#b91c1c',
+      'footer-color': '#b91c1c',
+      'promo-color' : '#fef08a',
+      'primario' : '#991b1b',
+      'secundario' : '#d97706',
+      'prod-desc' : '#374151',
+      'fondo' : '#a8a29e',
+    },
   },
   plugins: [],
 }
